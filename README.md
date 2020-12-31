d02f55c0f6b8bbbad40f9fabd92a7bfd0369c0b2
Pal Kerecsenyi <palkerecs@gmail.com>
2020-12-31 14:35:03 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typings for PalCode
+This repository contains a single source of truth for PalCode. It includes language names, database structures, and more. It also contains some helper functions, primarily to do with language data.
+
+## Installation
+```
+yarn add -D palcode-types
+```
---
+++ src/index.ts
@@ -0,0 +1,100 @@
+export type SupportedLanguage = 'python' | 'nodejs' | 'bash' | 'java' | 'prolog' | 'go' | 'cpp';
+
+export interface LanguageData {
+    names: {
+        display: string;
+        code: SupportedLanguage;
+        image: string;
+    }
+    extension: string;
+    entrypoint: string;
+}
+
+export const languageData = [
+    {
+        names: {
+            display: 'Python',
+            code: 'python',
+            image: 'python',
+        },
+        extension: '.py',
+        entrypoint: 'main.py',
+    },
+    {
+        names: {
+            display: 'Node.JS',
+            code: 'nodejs',
+            image: 'node',
+        },
+        extension: '.js',
+        entrypoint: 'index.js',
+    },
+    {
+        names: {
+            display: 'Bash',
+            code: 'bash',
+            image: 'bash',
+        },
+        extension: '.sh',
+        entrypoint: 'main.sh',
+    },
+    {
+        names: {
+            display: 'Java',
+            code: 'java',
+            image: 'java',
+        },
+        extension: '.java',
+        entrypoint: 'Main.java',
+    },
+    {
+        names: {
+            display: 'Prolog',
+            code: 'prolog',
+            image: 'prolog',
+        },
+        extension: '.pl',
+        entrypoint: 'main.pl',
+    },
+    {
+        names: {
+            display: 'Golang',
+            code: 'go',
+            image: 'go',
+        },
+        extension: '.go',
+        entrypoint: 'main.go',
+    },
+    {
+        names: {
+            display: 'C++',
+            code: 'cpp',
+            image: 'cpp',
+        },
+        extension: '.cpp',
+        entrypoint: 'main.cpp',
+    },
+] as LanguageData[];
+
+export const isValidLanguage = (
+    language: any,
+    by: 'code' | 'image' | 'display',
+): boolean => {
+    return languageData
+        .map(e => e.names[by])
+        .includes(language);
+}
+
+export enum ProjectType {
+    Template,
+    Submission,
+    Private,
+}
+export type TaskType = ProjectType;
+
+export enum ProjectStatus {
+    Unsubmitted,
+    Submitted,
+    HasFeedback,
+}
+export type TaskStatus = ProjectStatus;
